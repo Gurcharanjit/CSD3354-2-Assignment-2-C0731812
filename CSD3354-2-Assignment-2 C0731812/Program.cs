a660073f5a41e01d6f4c9ac02986ad303a3c474a
visualstudio <visualstudio@122-PC18>
2019-03-08 18:11:01 -0500

    Commit 1

--- CSD3354-2-Assignment-2 C0731812/Program.cs
+++ CSD3354-2-Assignment-2 C0731812/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Gurcharanjit Singh C0731812 // Harman Singh C0725563
+// CSD3354 Section 2
+// Assignment 2
+// March 8,2019
+
 namespace CSD3354_2_Assignment_2_C0731812
 {
     class Program
