ac1b337708c3e6b5a5ce1f060d1e9b9ce2023119
visualstudio <visualstudio@122-PC18>
2019-03-08 20:28:21 -0500

    lab 11 to report on the results of running this code

--- CSD3354-2-Assignment-2 C0731812/Program.cs
+++ CSD3354-2-Assignment-2 C0731812/Program.cs
@@ -7,32 +7,13 @@
 
 namespace CSD3354_2_Assignment_2_C0731812
 {
-    public class Program
-    {
-        public static void Main()
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
-        }
-    }
-
-    public delegate void MyDelegate();
-
     public class DelegateExercises
     {
-        void Method1()
+        public object MyDelegate { get; private set; }
+
+        void Method3()
         {
-            System.Console.WriteLine("Method1");
-
-
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-            System.Console.WriteLine(myDelegate.ToString());
+            Console.WriteLine(MyDelegate.ToString());
         }
     }
 }
